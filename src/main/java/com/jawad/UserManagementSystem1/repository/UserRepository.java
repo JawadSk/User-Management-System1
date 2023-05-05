e2d6d335bba0e5e4331a3fbe31704d615b36c61f
JawadSk <shaikhjawad12@gmail.com>
2023-05-05 17:20:40 +0530

    commit

---
+++ src/main/java/com/jawad/UserManagementSystem1/repository/UserRepository.java
@@ -0,0 +1,46 @@
+package com.jawad.UserManagementSystem1.repository;
+
+import com.jawad.UserManagementSystem1.model.UserManagement;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserRepository {
+
+    private List<UserManagement> userManagementList;
+
+    public UserRepository() {
+
+        userManagementList=new ArrayList<>();
+//        userManagementList.add(new UserManagement(1,"xyz","zyxsecsd","bluechip","1234567890"));
+    }
+
+    public  boolean save(UserManagement userManagement)
+    {
+        userManagementList.add(userManagement);
+        return true;
+    }
+
+    public List<UserManagement> getUsersFromDataSource()
+    {
+        return userManagementList;
+    }
+
+    public boolean update(int id,UserManagement userManagement) {
+        for(UserManagement userManage:userManagementList){
+            if(userManage.getUserId()==id){
+                remove(userManage);
+                save(userManagement);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean remove(UserManagement userManagement){
+        userManagementList.remove(userManagement);
+        return true;
+    }
+}
---
+++ src/test/java/com/jawad/UserManagementSystem1/UserManagementSystem1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jawad.UserManagementSystem1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementSystem1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
